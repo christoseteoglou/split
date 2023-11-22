66e9bdb946d66d5ca3ebb33b746082518e73c169
christoseteoglou <itsmeglue@gmail.com>
2023-11-22 12:08:03 +0100

    Initial Commit

---
+++ Split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Split
+//
+//  Created by Christos Eteoglou on 2023-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Split/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Split
+//
+//  Created by Christos Eteoglou on 2023-11-22.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
